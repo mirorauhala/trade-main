a9c4bf171c62701f49fd824d43ef9ebbcd5a4963
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-12-11 00:33:05 +0200

    Fix nav overlap

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="de">
       <Head />
-      <body className="bg-slate-50">
+      <body className="bg-slate-50 pb-24">
         <Main />
         <NextScript />
       </body>
